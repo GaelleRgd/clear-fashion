43d7ccf64fb986b071261f5f30aa8b074fe3971d
GaelleRgd <77722509+GaelleRgd@users.noreply.github.com>
2022-03-14 13:27:30 +0100

    Connection to vercel

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -45,7 +45,7 @@ const setCurrentProducts = ({result, meta}) => {
 const fetchProducts = async (page = 1, size = 12) => {
   try {
     const response = await fetch(
-      `https://clear-fashion-api.vercel.app?page=${page}&size=${size}`
+      `https://server-iota-beige.vercel.app?page=${page}&size=${size}`
     );
     const body = await response.json();
 
